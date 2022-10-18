12d47c1856b820850d6363d7d1746f6f0591c065
KEVIN <111806307+0529489@users.noreply.github.com>
2022-10-18 19:43:09 -0400

    Update HelloTest1.java
    Added initials and date on line 3, modified line 8.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 18 OCT 2022 kjl
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * My first pull request
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
